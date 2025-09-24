5acbe900cbadefe59307f12aaa285c011e072c3a
Eliadro_Dev <suzyneytandala@gmail.com>
2025-09-24 21:14:47 +0100

    index.java

---
+++ index.java
@@ -0,0 +1,8 @@
+meu-banco-js5/
+├─ data/                     <-- pasta onde ficará o arquivo do DB (crie vazia)
+├─ controllers/
+│  └─ userController.js
+├─ models/
+│  └─ userModel.js
+├─ db.js
+└─ index.js
